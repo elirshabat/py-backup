3d229f6a0ecb354d4c91ab942d3dd35430926b54
Eliran Shabat <shabat.eliran@gmail.com>
2019-12-21 22:35:04 +0200

    Add setup.py and licence files

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pybackup",
+    version="0.0.1",
+    author="Eliran Shabat",
+    author_email="shabat.eliran@gmail.com",
+    description="Python package for backup",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/elirshabat/py-backup",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
