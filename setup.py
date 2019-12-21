f1dd5008d51b66ac7b0f8869eeac442a5b3466f4
Eliran Shabat <shabat.eliran@gmail.com>
2019-12-21 22:45:11 +0200

    Update setup.py - specify licence; add dependecies

--- setup.py
+++ setup.py
@@ -8,11 +8,16 @@ setuptools.setup(
     version="0.0.1",
     author="Eliran Shabat",
     author_email="shabat.eliran@gmail.com",
+    license='MIT',
     description="Python package for backup",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/elirshabat/py-backup",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'PyYAML',
+        'tqdm'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
